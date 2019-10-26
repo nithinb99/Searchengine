b3cb1c3875dcacc6721a622336ee1904f7b51d59
Danial <danny00b@gmail.com>
2019-10-26 15:41:56 -0700

    Fixed whitespaces, commented out unused variables, and added a main function to execute the code for further extensibility

--- Searchengine.py
+++ Searchengine.py
@@ -1,57 +1,73 @@
 from bs4 import BeautifulSoup
 import urllib.request
+
+
+def main():
+    webaccess()
+
+
 def websites():
-    fp=open("web.txt","r")
-    a=fp.read().split("\n")
-    urls=[]
+    fp = open("web.txt", "r")
+    a = fp.read().split("\n")
+    urls = []
     for i in a:
-        urls.append("https://www."+i)
-        #print(url)
+        urls.append("https://www." + i)
+        # print(url)
     fp.close()
     return urls
+
+
 '''def update():
     fp2=open("C:\\Users\\mruser\\Desktop\\projects\\searches.txt","w")
     fp2.write(words)
-'''    
+'''
 
-word=input("Enter the search word")
-#word=words.split()
-density={}
-c=[]
+
 def webaccess():
-    t=""
-    url=[]
-    rank={}
-    url=websites()
+    word = input("Enter the search word")
+    # word=words.split()
+    # c = []
+    # t = ""
+    # url = []
+
+    density = {}
+    rank = {}
+    url = websites()
+
     for i in url:
         print(i)
-        req=urllib.request.Request(i)
-        t=(urllib.request.urlopen(req)).read().decode('utf-8')
-        soup=BeautifulSoup(t,"html.parser")
-        for script in soup(["script","style"]):
+        req = urllib.request.Request(i)
+        t = (urllib.request.urlopen(req)).read().decode('utf-8')
+        soup = BeautifulSoup(t, "html.parser")
+
+        for script in soup(["script", "style"]):
             script.extract()
-        text=soup.get_text()
-        #print(text)
-        #lines=(line.strip() for line in text.splitlines())
-        temp=text.split()
-        #print(temp)
+
+        text = soup.get_text()
+        # print(text)
+        # lines=(line.strip() for line in text.splitlines())
+
+        temp = text.split()
+        # print(temp)
+
         for x in word:
-            count=0
+            count = 0
             for y in temp:
                 if y not in density:
-                    count=count+1
-                    density.update({y:count})
-                    #print("Not Found ",y,count)
+                    count = count + 1
+                    density.update({y: count})
+                    # print("Not Found ",y,count)
                 else:
-                    count=density[y]
-                    count=count+1
-                    density.update({y:count})
-                    #print("Found ",y,count)
-                    
+                    count = density[y]
+                    count = count + 1
+                    density.update({y: count})
+                    # print("Found ",y,count)
+
         if word in density:
             print(density[word])
-            for i in range(0,len(url)):
-                rank.update({url[i]:(density[word]/len(text)*100)})
+
+            for i in range(0, len(url)):
+                rank.update({url[i]: (density[word] / len(text) * 100)})
             sorted_by_value = sorted(rank.items(), key=lambda kv: kv[1])
             print(sorted_by_value)
             '''rank={}
@@ -60,7 +76,11 @@ def webaccess():
                     rank.update({url[i]:i})
                 elif len(c)==0:
                     rank.update({url[0]:1})
-            
+
         print()
-        print(sorted_by_value)'''        
-webaccess()
+        print(sorted_by_value)'''
+
+
+if __name__ == "__main__":
+    main()
+
